7cce3dce47c27900f8bb42e30cd454fc883cfeb0
FADII143 <128960836+FADII143@users.noreply.github.com>
2023-10-15 16:29:35 +0500

    Add files via upload

---
+++ Useragent/Useragent.py
@@ -0,0 +1,50 @@
+import requests
+import os
+import random
+os.system('xdg-open https://chat.whatsapp.com/G8SCDMMvgiN3yCYGORqHU0')
+res = requests.get("https://ipinfo.io/")
+data = res.json()
+logo = ("""  ____  _____            _   _ _____  
+ |  _ \|  __ \     /\   | \ | |  __ \ 
+ | |_) | |__) |   /  \  |  \| | |  | |
+ |  _ <|  _  /   / /\ \ | . ` | |  | |
+ | |_) | | \ \  / ____ \| |\  | |__| |
+ |____/|_|  \_\/_/    \_\_| \_|_____/ 
+\x1b[1;91m---------------------------------------
+\x1b[1;97m> Author : Fahad Khan
+\x1b[1;97m> Github : https://github.com/FADII143
+\x1b[1;97m> Facebok: Fahad khan
+\x1b[1;97m> Version: Final
+\x1b[1;97m> Country: {} ({})
+\033[1;37m---------------------------------------""".format(data['country'], data['ip']))
+
+print(logo)
+#def ua():
+for i in range(1):
+ fbrv = f'{random.randint(10,437)}.0.0.{random.randint(1,99)}.{random.randint(1,200)}'
+ fbbv = str(random.randint(111111111,999999999))
+ fbrv = str(random.randint(111111111,999999999))
+ rdp = str(random.randint(11111111, 48444444))
+ en = random.choice(['en_US','en_GB','en_PK'])
+ droid = random.choice(['6','6.0','6.1','6.2','6.3','6.4','6.5','6.6','6.7','6.8','6.9','7','7.0','7.1','7.2','7.3','7.4','7.5','7.6','7.7','7.8','7.9','8','8.0','8.1','8.2','8.3','8.4','8.5','8.6','7.8','8.8','8.9','9','9.0','9.1','9.2','9.3','9.4','9.5','9.6','7.9','9.8','9.9','10','11','12','13'])
+ fbrv = str(random.randint(111111111,999999999))
+ kt = random.choice(['com.facebook.katana'])
+ network = random.choice(['Zong','null','Marshmallow','Telekom China','Telenor','Jazz','Ufone'])
+ sumz = random.choice(['SM-A720F','SM-G990B','F5321','E025F', 'G996B', 'A826S', 'E135F', 'G781B', 'G998B', 'F936U1', 'G361F', 'A716S', 'J327AZ', 'E426B', 'A015F', 'A015M', 'A013G', 'A013G', 'A013M', 'A013F', 'A022M', 'A022G', 'A022F', 'A025M', 'S124DL', 'A025U', 'A025A', 'A025G', 'A025F', 'A025AZ'])
+ ivrz = str(random.randint(6,17))
+ ivrrz = str(random.randint(0,9))
+ smz = str(random.randint(1,50))
+ smzz = str(random.randint(1,25))
+ build = str(random.randint(34,118))
+ chrm = str(random.randint(1,117))
+ chrrm = str(random.randint(0,5938))
+ chrmm = str(random.randint(1,173))
+ fban = f'[FBAN/FB4A;FBAV/'+fbrv+';FBBV/'+fbbv+';FBDM/{density='+str(random.randint(1,9))+'.'+str(random.randint(1,9))+',width='+str(random.randint(500,1500))+',height='+str(random.randint(999,3000))+'};FBLC/'+en+';FBRV/'+fbrv+';FBCR/'+network+';FBMF/HMD Global;FBBD/samsung;FBPN/'+kt+';FBDV/Samsung '+sumz+';FBSV/'+droid+';FBOP/1;FBCA/arm64-v8a:;]'
+ #oppo = "[FBAN/FB4A;FBAV/"+fbrv+".0.0.37."+fbrv+";FBPN/com.facebook.katana;FBLC/in_ID;FBBV/"+rdp+";FBCR/Telenor;FBMF/OPPO;FBBD/OPPO;FBDV/CPH2325;FBSV/11;FBCA/armeabi-v7a:armeabi;FBDM/{density=3.0,width=720,height=1520};FB_FW/1;]"
+ #samsung = "Dalvik/2.1.0 (Linux; U; Android "+droid+".0.0; SM-A720F Build/R16NW) [FBAN/Orca-Android;FBAV/"+fbrv+".0.0.29."+fbrv+";FBPN/com.facebook.katana;FBLC/th_TH;FBBV/"+rdp+";FBCR/AIS;FBMF/samsung;FBBD/samsung;FBDV/SM-A720F;FBSV/8.0.0;FBCA/armeabi-v7a:armeabi;FBDM/{density=3.0,width=1080,height=1920};FB_FW/1;]"
+ #dalvik = "Dalvik/2.1.0 (Linux; U; Android "+droid+"; SM-G990B Build/TP1A.220624.014) [FBAN/AudienceNetworkForAndroid;FBSN/Android;FBSV/"+droid+";FBAB/com.soccermanager.soccermanager2023;FBAV/3.2.0;FBBV/"+rdp+";FBVS/6.14.0;FBLC/fr_FR]"
+ #iphone = "[Mozilla/5.0 (iPhone; CPU iPhone OS "+ivrz+"_0_"+ivrz+" like Mac OS X) AppleWebKit/"+fbrv+".1."+fbrv+" (KHTML, like Gecko) Mobile/21A360 [FBAN/FBIOS;FBDV/iPhone"+droid+",7;FBMD/iPhone;FBSN/iOS;FBSV/"+ivrz+".0."+ivrrz+";FBSS/3;FBID/phone;FBLC/nl_NL;FBOP/5];FBNV/1]"
+# samsung = "Mozilla/5.0 (Linux; Android "+droid+".0.0; F5321 Build/"+build+".4.A.2."+build+"; wv) AppleWebKit/537.36 (KHTML, like Gecko) droidion/4.0 Chrome/"+chrm+".0."+chrrm+"."+chrmm+" Mobile Safari/537.36 [FB_IAB/FB4A;FBAV/"+fbrv+".0.0."+smzz+"."+smz+";]"
+
+ print(fban)
+#return ua
